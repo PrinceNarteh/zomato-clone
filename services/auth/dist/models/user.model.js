a8a5bf71acd864e445da90100f8c4ac83e78f726
Prince Narteh <prinarteh@gmail.com>
2026-05-03 21:22:27 +0000

    feat: add user model

---
+++ services/auth/dist/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    role: {
+        type: String,
+        default: null,
+    },
+}, {
+    timestamps: true,
+});
+const User = mongoose.model("User", userSchema);
+export default User;
---
+++ services/auth/src/models/user.model.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  image: string;
+  role: string;
+}
+
+const userSchema: Schema<IUser> = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    role: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model<IUser>("User", userSchema);
+export default User;
